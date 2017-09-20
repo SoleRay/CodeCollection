efb411a706ad3428ced5946158ff8393a8412925
SoleRay <pein_zero@126.com>
2017-09-20 15:39:51 +0800

    re-init

---
+++ src/main/java/express/ExpressKd100Utils.java
@@ -0,0 +1,22 @@
+package express;
+
+import http.HttpClientUtil;
+
+import java.text.MessageFormat;
+
+public class ExpressKd100Utils {
+
+    private static String URL_TEMPLATE = "http://www.kuaidi100.com/query?type={0}&postid={1}";
+
+
+    public static String getTraces(String shipperCode, String logisticCode) throws Exception{
+
+        String queryUrl = MessageFormat.format(URL_TEMPLATE, shipperCode, logisticCode);
+        String s = HttpClientUtil.doGet(queryUrl);
+        return s;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(getTraces("shunfeng","139246905772"));
+    }
+}
