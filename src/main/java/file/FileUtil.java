13f3dce60e19f121b51a9024a4a1f7edcc07129d
SoleRay <pein_zero@126.com>
2017-11-09 09:43:25 +0800

    re-init

--- src/main/java/file/FileUtil.java
+++ src/main/java/file/FileUtil.java
@@ -1,8 +1,19 @@
 package file;
 
+import java.io.InputStream;
+
 public class FileUtil {
 
     public static String getFileSuffix(String fileName){
         return fileName.substring(fileName.lastIndexOf(".")+1);
+    }
+
+
+    public static String getFileNameUnderResources(String fileName){
+        return FileUtil.class.getClassLoader().getResource(fileName).getFile();
+    }
+
+    public static InputStream getFileStreamUnderResources(String fileName){
+        return FileUtil.class.getClassLoader().getResourceAsStream(fileName);
     }
 }
