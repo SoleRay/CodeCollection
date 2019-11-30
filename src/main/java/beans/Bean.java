525ad921174409805d6f4251c58925d07ee42bf4
soleray <pein_zero@126.com>
2019-11-30 20:15:03 +0800

    queue

---
+++ src/main/java/beans/Bean.java
@@ -0,0 +1,52 @@
+package beans;
+
+public class Bean {
+
+    private int attr1;
+
+    private String attr2;
+
+    private Bean attr3;
+
+    public Bean() {
+    }
+
+    public Bean(int attr1, String attr2, Bean attr3) {
+        this.attr1 = attr1;
+        this.attr2 = attr2;
+        this.attr3 = attr3;
+    }
+
+    public int getAttr1() {
+        return attr1;
+    }
+
+    public void setAttr1(int attr1) {
+        this.attr1 = attr1;
+    }
+
+    public String getAttr2() {
+        return attr2;
+    }
+
+    public void setAttr2(String attr2) {
+        this.attr2 = attr2;
+    }
+
+    public Bean getAttr3() {
+        return attr3;
+    }
+
+    public void setAttr3(Bean attr3) {
+        this.attr3 = attr3;
+    }
+
+    @Override
+    public String toString() {
+        return "Bean{" +
+                "attr1=" + attr1 +
+                ", attr2='" + attr2 + '\'' +
+                ", attr3=" + attr3 +
+                '}';
+    }
+}
---
+++ src/main/java/beans/BeanAndMapConvert.java
@@ -0,0 +1,30 @@
+package beans;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BeanAndMapConvert {
+
+    public static void beanToMap() throws Exception{
+        Bean bean = new Bean(1,"box",new Bean());
+        Map<String,String> map = BeanUtils.describe(bean);
+        System.out.println(map.get("attr3"));
+    }
+
+    public static void mapToBean() throws Exception{
+        Bean bean = new Bean();
+        Map<String,String> map = new HashMap<>();
+        map.put("attr1","1");
+        map.put("attr2","cup");
+        BeanUtils.populate(bean,map);
+        System.out.println(bean);
+    }
+
+    public static void main(String[] args) throws Exception {
+//        beanToMap();
+        mapToBean();
+    }
+
+}
