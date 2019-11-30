b21053eca5e2c693826ec81760be47d6231df0b9
soleray <pein_zero@126.com>
2019-11-30 20:32:42 +0800

    queue

--- src/main/java/file/FileUtil.java
+++ src/main/java/file/FileUtil.java
@@ -1,6 +1,11 @@
 package file;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class FileUtil {
 
@@ -16,4 +21,20 @@ public class FileUtil {
     public static InputStream getFileStreamUnderResources(String fileName){
         return FileUtil.class.getClassLoader().getResourceAsStream(fileName);
     }
+
+
+    public static void copyFile(String srcFileName,String destFileName) throws IOException {
+        FileUtils.copyFile(new File(srcFileName),new File(destFileName));
+    }
+
+    public static List<String> readLines(String fileName) throws IOException {
+        List<String> list = FileUtils.readLines(new File(fileName), "UTF-8");
+        return list;
+    }
+
+    public static void writeStringToFile(String fileName,String str) throws IOException {
+        FileUtils.writeStringToFile(new File(fileName),str,"UTF-8");
+    }
+
+
 }
